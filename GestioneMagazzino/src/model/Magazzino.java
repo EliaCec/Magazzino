30d58fc13e36ee338240d2072c185eae8a903a07
Elia C <e.ceccolini2@campus.uniurb.it>
2022-09-12 12:19:23 +0200

    Aggiornamento interfaccia Magazzino

--- GestioneMagazzino/src/model/Magazzino.java
+++ GestioneMagazzino/src/model/Magazzino.java
@@ -1,5 +1,16 @@
 package model;
 
-public interface Magazzino {
+import java.util.Date;
+import java.util.LinkedList;
 
+public interface Magazzino {
+	public int getNumeroReparti();
+	public LinkedList<ProdottoFinito> storicoVenditeGiornaliero(Date giorno);
+	public LinkedList<ProdottoFinito> storicoCostruzioniGiornaliero(Date giorno);
+	public LinkedList<Semilavorato> storicoSemilavoratiUsatiGiornaliero(Date giorno);
+	public LinkedList<Responsabile> getResponsabiliAttivi();
+	public LinkedList<Operaio> getOperaiAttivi();
+	public boolean cambioTurno(LinkedList<Operaio> operai, 
+							   LinkedList<Responsabile> responsabili,
+							   Date giorno);
 }
