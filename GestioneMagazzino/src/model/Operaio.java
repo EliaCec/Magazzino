a4ac082b1c3b1ccc512942852d43ff640959195b
inri30sema <i.sema1@campus.uniurb.it>
2022-09-12 15:01:21 +0200

    inserimento metodi delle seguenti interfacce: Gestione operai, Operaio e Prodotto finito

--- GestioneMagazzino/src/model/Operaio.java
+++ GestioneMagazzino/src/model/Operaio.java
@@ -1,5 +1,39 @@
 package model;
 
-public interface Operaio {
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedList;
 
+public interface Operaio {
+	
+	/* Metodo che restituisce il nome e il cognome dell'operaio */
+	public String getNomeCognome();
+	
+	/* Metodo che restituisce l'ID dell'operaio */
+	public int getId();
+	
+	/* Metodo che serve per estrarre tutti i prodotti finiti costruiti.
+	 * Restituisce la relativa lista di prodotti finiti */ 
+	public LinkedList<ProdottoFinito> getProdottiCostruiti();
+	
+	/* Metodo che serve per estrarre tutti i semilavorati utilizzati.
+	 * Restituisce la relativa lista di semilavorati */ 
+	public LinkedList<Semilavorato> getSemilavoratiPrelevati();
+	
+	/* Metodo che serve per costruire un prodotto finito (pf).
+	 * Prende in input la mappa dei prodotti finiti da costruire, l'operaio responsabile alla costruzione e la giornata.
+	 * Il metodo preleva anche i semilavorati necessari alla costruzione del prodotto finito.
+	 * Restituisce true se la costruzione è stata effettuata con successo */
+	public boolean costruisciProdottiFiniti(HashMap<ProdottoFinito, Integer> pf, Operaio operaio, Date giorno);
+	
+	/* Metodo che restituisce il numero totale di prodotti finiti costruiti */
+	public int costruzioniPerProdottoFinito(ProdottoFinito pf);
+	
+	/* Metodo che restituisce il numero totale dei semilavorati prelevati */
+	public int prelievoPerSemilavorato(Semilavorato sl);
+	
+	/* Metodo che restituisce il numero di prodotti finiti ancora costruibili 
+	 * basandosi sui semilavorati rimanenti in magazzino */
+	public int calcoloProdottiFinitiCostruibili(ProdottoFinito pf);
+	
 }
--- GestioneMagazzino/src/model/ProdottoFinito.java
+++ GestioneMagazzino/src/model/ProdottoFinito.java
@@ -1,5 +1,11 @@
 package model;
 
-public interface ProdottoFinito extends Giacenza{
+import java.util.HashMap;
+import java.util.LinkedList;
 
+public interface ProdottoFinito extends Giacenza{
+	
+	/* Metodo che restituisce la mappa dei semilavorati necessari alla costruzione
+	 * di un prodotto finito. */
+	public HashMap<Semilavorato, Integer> getComponenti();
 }
