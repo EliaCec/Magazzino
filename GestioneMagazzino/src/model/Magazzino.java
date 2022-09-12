0606ad6f3440fc1c6a608d1a56ff498556a1e5df
Elia C <e.ceccolini2@campus.uniurb.it>
2022-09-12 12:52:04 +0200

    Aggiunti commenti interfaccia Magazzino, aggiornamento interfaccia Reparto

--- GestioneMagazzino/src/model/Magazzino.java
+++ GestioneMagazzino/src/model/Magazzino.java
@@ -4,12 +4,32 @@ import java.util.Date;
 import java.util.LinkedList;
 
 public interface Magazzino {
-	public int getNumeroReparti();
+	/* Metodo che restituisce il numero totale dei reparti presenti nel magazzino */
+	public int getNumeroReparti(); 
+	
+	/* Metodo che serve per estrarre tutti i prodotti finiti che sono stati venduti in
+	 * un determinato giorno nell'intero magazzino.
+	 * Restituisce la relativa lista di prodotti finiti */
 	public LinkedList<ProdottoFinito> storicoVenditeGiornaliero(Date giorno);
+	
+	/* Metodo che serve per estrarre tutti i prodotti finiti che sono stati costruiti
+	 * in un determinato giorno nell'intero magazzino.
+	 * Restituisce la relativa lista di prodotti finiti */
 	public LinkedList<ProdottoFinito> storicoCostruzioniGiornaliero(Date giorno);
+	
+	/* Metodo che serve per estrarre tutti i semilavorati consumati in un determinato giorno
+	 * nell'intero magazzino.
+	 * Restituisce la relativa lista di semilavorati */
 	public LinkedList<Semilavorato> storicoSemilavoratiUsatiGiornaliero(Date giorno);
+	
+	/* Metodo che restituice i responsabili che stanno lavorando in questo momento nel magazzino */
 	public LinkedList<Responsabile> getResponsabiliAttivi();
+	
+	/* Metodo che restituisce gli operai che stanno lavorando in questo momento nel magazzino */
 	public LinkedList<Operaio> getOperaiAttivi();
+	
+	/* Metodo che serve per effettuare il cambio turno degli impiegati del magazzino.
+	 * Restituisce true se il cambio di turno è avvenuto con successo, false altrimenti */
 	public boolean cambioTurno(LinkedList<Operaio> operai, 
 							   LinkedList<Responsabile> responsabili,
 							   Date giorno);
--- GestioneMagazzino/src/model/Reparto.java
+++ GestioneMagazzino/src/model/Reparto.java
@@ -1,5 +1,32 @@
 package model;
 
 public interface Reparto {
-
+	/* Metodo che restituisce il codice univoco che identifica il reparto */
+	public int getId();
+	
+	/* Metodo che restituisce il nome del reparto */
+	public String getNome();
+	
+	/* Metodo che restituisce la quantità di scorte presenti nel reparto corrente */
+	public int getQuantita();
+	
+	/* Metodo che restituisce la capacità massima del reparto corrente */
+	public int getCapacita();
+	
+	/* Metodo che serve per verificare la presenza di almeno una scorta nel reparto corrente.
+	 * Restituisce false nel caso in cui il reparto corrente sia vuoto, true altrimenti */
+	public boolean isPresente();
+	
+	/* Metodo che serve per verificare la saturità del reparto corrente (se la capacità
+	 * massima è stata raggiunta).
+	 * Restituisce true nel caso in cui il reparto corrente sia pieno, false atrimenti */
+	public boolean isPieno();
+	
+	/* Metodo usato per depositare un numero n di scorte dal reparto corrente.
+	 * Restituisce true nel caso in cui il deposito avvenga con successo, false altrimenti */
+	public boolean depositaScorte(int n);
+	
+	/* Metodo usato per prelevare un numero n di scorte dal reparto corrente.
+	 * Restituisce true nel caso in cui il prelievo avvenga con successo, false altrimenti */
+	public boolean prelevaScorte(int n);
 }
